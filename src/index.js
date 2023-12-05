97fb321a504c943e9ca7007a090ae3892c536de0
heatherleeriley <heatherleeriley@yahoo.com>
2023-12-04 19:17:33 -0500

    injected the forecast HTML to JS, added forecast CSS

--- src/index.js
+++ src/index.js
@@ -54,7 +54,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+            <div class="weather-forecast-day">            
+              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forcast-icon">🌥</div>
+              <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature"><strong>18°</strong></div>
+                <div class="weather-forecast-temperature">12°</div>
+            </div>
+          </div> 
+          `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Saugatuck");
+
+displayForecast();
